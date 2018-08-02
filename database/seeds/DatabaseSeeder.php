bb6a1e28992b2f240a8960040f7e9f34847abe31
phanvantan73 <malong188@gmail.com>
2018-08-02 10:45:57 +0700

    Seeding data with hashing password

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(ActivitiesTableSeeder::class);
+        $this->call(CoursesTableSeeder::class);
+        $this->call(LessonTableSeeder::class);
+        $this->call(ListOfWordsTableSeeder::class);
+        $this->call(OptionsTableSeeder::class);
+        $this->call(ProfilesTableSeeder::class);
+        $this->call(QuestionsTableSeeder::class);
+        $this->call(RelationshipsTableSeeder::class);
+        $this->call(ResultsTableSeeder::class);
+        $this->call(RolesTableSeeder::class);
+        $this->call(SocialsTableSeeder::class);
     }
 }
