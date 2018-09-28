7e71b735bf096f2d164e85afd008b77412b3ee19
phanvantan73 <malong188@gmail.com>
2018-09-28 15:01:37 +0700

    add word to list

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -4,10 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Models\Course;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Http\Request;
+use App\Models\Word;
+use DB;
+use App\Traits\CreateActivity;
+use Exception;
 
 class CourseController extends Controller
 {
+    use CreateActivity;
     /**
      * Display a listing of the resource.
      *
@@ -34,5 +38,25 @@ class CourseController extends Controller
         }
 
         return view('courses.show', compact('course'));
+    }
+
+    public function addWordToList($wordId, $courseId)
+    {
+        try {
+            $word = Word::find($wordId);
+            DB::transaction(function () use ($wordId, $word) {
+                $this->user->words()->attach($wordId, [
+                    'status' => 0,
+                    'add_to_list_time' => now(),
+                    'learn_time' => null,
+                ]);
+
+                $this->createActivity($this->user, trans('message.action.add'), trans('message.default.word'), $word->word);
+            });
+
+            return redirect()->route('courses.show', $courseId);
+        } catch (Exception $e) {
+            return back()->with('notFoundError', trans('message.notFoundError'));
+        }
     }
 }
